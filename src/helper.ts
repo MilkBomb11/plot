544f03588dc00d4c24498f8f15b4de01cc7e43b1
MilkBomb11 <milkbomb11@gmail.com>
2026-03-24 00:19:57 +0900

    update helper

--- src/helper.ts
+++ src/helper.ts
@@ -1,12 +1,3 @@
 function assertUnreachable (_x: never): never { throw new Error("Non exhaustive match"); }
 
-function getBit (n: number, pos: number) {
-    return (n >> pos) & 1;
-}
-
-function setBit (n: number, pos: number, value: boolean) {
-    if (value) { return n | (1 << pos); }
-    else { return n & ~(1 << pos); }
-}
-
-export { assertUnreachable, getBit, setBit }
+export { assertUnreachable }
